5801443e9f8cb2c46fa11bbe0ec5f5628ae735bb
shido3901 <kracing804@gmail.com>
2025-08-13 13:48:49 -0700

    updated init

--- init_booru.py
+++ init_booru.py
@@ -1,16 +1,19 @@
 
 
 
-from PyQt5.QtWidgets import (QMainWindow, QApplication, QWidget, QSizePolicy,
-                             QVBoxLayout, QHBoxLayout)
+from PyQt5.QtWidgets import (QMainWindow, QApplication, QWidget, QVBoxLayout)
 from PyQt5.QtCore import Qt
 
-from config.app_configuration import ApplicationConfiguration as Config
-from config.booru_ui import LightWidget, DarkWidget
+from db.booru_db import BooruDb
+from config.app_configuration import ApplicationConfiguration as AppConfig
+
+from config.settings_bar import SettingsBar
 
 from media_properties.media_bar import MediaBar
 from media_properties.media_viewer import MediaViewer
 from media_properties.tab_bar import TabBar
+
+from config.booru_ui import LightWidget, DarkWidget, InformationBar, message_signal
 
 import sys
 
@@ -22,13 +25,29 @@ QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps)
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-      
-        app_config = Config(main_window=self)
-        app_config.init_config(DEBUG_MODE)
-        self.init_ui()
 
-    def resizeEvent(self, a0):
-        return super().resizeEvent(a0)
+        self.booru_db   = BooruDb(main_window=self)
+        self.app_config = AppConfig(main_window=self, db=self.booru_db, DEBUG_MODE=DEBUG_MODE)
+
+        self.profile_config = self.app_config.profile_config
+
+        self.init_ui()     
+        self.profile_config.load_profile()
+
+    def init_profile_info(self):
+
+        self.tab_bar.load_tabs()
+       
+    def clear_ui(self):
+        self.setWindowTitle("BooruSort")
+
+        self.tab_bar.clear()
+
+    def closeEvent(self, event):
+        self.app_config.save_window_state()
+        
+        super().closeEvent(event)
+
         
     def init_ui(self):
        
@@ -71,43 +90,42 @@ class MainWindow(QMainWindow):
        
         bottom_area.setMinimumHeight(30)
 
-        #settings widget
-
-        tab_bar      = TabBar()
-        media_area   = QWidget()
+        settings_bar = SettingsBar(self.profile_config)
+        self.tab_bar = TabBar     (self.profile_config)
+        media_area   = QWidget    (          )
         media_viewer = MediaViewer(media_area)
-        media_bar    = MediaBar(main_window, media_viewer)
+        media_bar    = MediaBar   (main_window, media_viewer)
+        notification = InformationBar()
+
+        
+       
+        media_area_layout = QVBoxLayout(media_area)
 
         media_area.setStyleSheet("border-top-left-radius: 0px; border-top-right-radius: 0px;")
 
-
-        media_area_layout = QVBoxLayout(media_area)
-        
-
-
-        tabs_area._layout  .addWidget(tab_bar)
+        settings._layout   .addWidget(settings_bar)
+        tabs_area._layout  .addWidget(self.tab_bar)
         center_area._layout.addWidget(media_bar, 1)
+        bottom_area._layout.addWidget(notification)
 
         center_area._layout.addWidget(media_area, 50)
         media_area_layout  .addWidget(media_viewer)
 
-
+        settings._layout   .setContentsMargins(0,0,0,0)
         tabs_area._layout  .setContentsMargins(0,0,0,0)
+        bottom_area._layout.setContentsMargins(0,0,0,0)
 
         center_area._layout.setContentsMargins(0,0,0,0)
 
         center_area._layout.setSpacing(0)
 
-        
-     
+        message_signal.text.connect(notification.change_text)
 
-    
-        
-        #media_display_area.setStyleSheet("background-color: white;")"""
+         #media_display_area.setStyleSheet("background-color: white;")"""
 
-    
-  
-       
+    def mousePressEvent(self, a0):
+        self.setFocus()
+        return super().mousePressEvent(a0)
 
 def main():
 
